0d75f411b09d40fb3839e7122bf2dc2165cf7631
Oleg Dmitrochenko <o.dmitrochenko@general-soft.by>
2021-04-20 23:06:07 +0300

    silence

---
+++ src/Silence.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SegmentGenerator;
+
+use SegmentGenerator\Contracts\Duration;
+
+/**
+ * Keeps intervals of a silence.
+ */
+class Silence implements Duration
+{
+    protected $from;
+    protected $until;
+
+    public function __construct(Interval $from, Interval $until)
+    {
+        $this->from = $from;
+        $this->until = $until;
+    }
+
+    /**
+     * Returns a 'from' interval of the silence.
+     *
+     * @return Interval
+     */
+    public function getFrom(): Interval
+    {
+        return $this->from;
+    }
+
+    /**
+     * Returns an 'until' interval of the silence.
+     *
+     * @return Interval
+     */
+    public function getUntil(): Interval
+    {
+        return $this->until;
+    }
+
+    /**
+     * Returns a duration of the silence.
+     *
+     * @return int
+     */
+    public function getDuration(): int
+    {
+        return $this->until->difference($this->from);
+    }
+}
